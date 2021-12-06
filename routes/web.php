740494bc1f4cffcdad7ed06bbb8a77dc7c357230
zeynepkiymik <33024264+zeynepkiymik@users.noreply.github.com>
2021-12-06 10:33:44 +0300

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', [HomeController::class , 'index'])->name('homepage');
+
+/*
+Route::get('/', function () {
+    return view('index');
+}); */
+
+Route::get('/about', [HomeController::class , 'about'])->name('about');
+/*
+Route::get('/about', function () {
+    return view('about');
+});
+*/
+Route::get('/post', [HomeController::class , 'post'])->name('post');
+
+/*
+Route::get('/post', function () {
+    return view('post');
+});
+*/
+Route::get('/contact', [HomeController::class , 'contact'])->name('contact');
+
+/*
+Route::get('/contact', function () {
+    return view('contact');
+});*/
